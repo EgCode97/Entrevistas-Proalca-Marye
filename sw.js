f6fc23718e941ce9d55b2f5273700be49b1ff85a
Elias Colmenarez <65724530+EgCode97@users.noreply.github.com>
2025-08-04 21:48:59 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'app-entrevistas-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './manifest.json',
+    './icono-192x192.png',
+    './icono-512x512.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Cache opened');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
